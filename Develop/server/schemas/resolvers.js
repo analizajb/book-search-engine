ef6a0230354e6b475f25cee4f0720f4efca0504f
analizajb <96861913+analizajb@users.noreply.github.com>
2022-08-12 01:35:48 -0700

    updating Query to allow login functionality in resolvers.js

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,22 +1,22 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Book } = require('../models');
-const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 
 const resolvers = {
   Query: {
     myUser: async (parent, args, context) => {
-      if(context.user) {
+      if (context.user) {
         const userData = await User.findOne({})
-        .select('-__v -password')
-        .populate('books')
+          .select("-__v -password")
+          .populate("books");
 
         return userData;
       }
-      throw new AuthenticationError("Doesn't look like you're logged in.")
-    }
+      throw new AuthenticationError("Doesn't look like you're logged in.");
+    },
   },
-  
+
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
@@ -24,48 +24,24 @@ const resolvers = {
 
       return { token, user };
     },
-    addOrder: async (parent, { products }, context) => {
-      console.log(context);
-      if (context.user) {
-        const order = new Order({ products });
 
-        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
-
-        return order;
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    updateUser: async (parent, args, context) => {
-      if (context.user) {
-        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    updateProduct: async (parent, { _id, quantity }) => {
-      const decrement = Math.abs(quantity) * -1;
-
-      return await Product.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
-    },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
-
       return { token, user };
-    }
-  }
+    },
+  },
 };
 
 module.exports = resolvers;
