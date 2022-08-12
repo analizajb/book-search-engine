ab93515907c0df80ef7a48561e38eaaa725ddac6
analizajb <96861913+analizajb@users.noreply.github.com>
2022-08-12 12:52:53 -0700

    working on typeDefs - inputting mod examples to get started

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -3,9 +3,46 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
-  }
+type User {
+  _id: ID
+  firstName: String
+  lastName: String
+  email: String
+  orders: [Order]
+}
+
+type Query {
+  categories: [Category]
+  products(category: ID, name: String): [Product]
+  product(_id: ID!): Product
+  user: User
+  order(_id: ID!): Order
+  checkout(products: [ID]!): Checkout
+}
+
+type Mutation {
+  addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+  addOrder(products: [ID]!): Order
+  updateUser(firstName: String, lastName: String, email: String, password: String): User
+  updateProduct(_id: ID!, quantity: Int!): Product
+  login(email: String!, password: String!): Auth
+}
+
+type Auth {
+  token: ID
+  user: User
+}
+
+type Product {
+  _id: ID
+  name: String
+  description: String
+  image: String
+  quantity: Int
+  price: Float
+  category: Category
+}
+
 `;
 
 // export the typeDefs
